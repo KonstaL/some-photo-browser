e8d26fa2b15ba3947deed8eb672f8bd07207f454
BuildTools <lehtinen.konsta@gmail.com>
2019-10-09 20:18:05 +0300

    add super basic header

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -3,10 +3,13 @@ import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { HeaderComponent } from './components/header/header.component';
+import { AppRoutingModule } from 'app/app-routing.module';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
-  declarations: [PageNotFoundComponent],
-  imports: [CommonModule, HttpClientModule, MatSlideToggleModule],
-  exports: [PageNotFoundComponent, MatSlideToggleModule],
+  declarations: [PageNotFoundComponent, HeaderComponent],
+  imports: [CommonModule, HttpClientModule, AppRoutingModule, MatSlideToggleModule, MatToolbarModule],
+  exports: [PageNotFoundComponent, MatSlideToggleModule, HeaderComponent],
 })
 export class SharedModule {}
