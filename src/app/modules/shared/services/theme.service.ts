153a1cc19edaa54160fa0e479421fbd9e83e4485
BuildTools <lehtinen.konsta@gmail.com>
2019-10-10 02:13:42 +0300

    Make photo detail scale

--- src/app/modules/shared/services/theme.service.ts
+++ src/app/modules/shared/services/theme.service.ts
@@ -4,11 +4,19 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class ThemeService {
-  constructor() {}
+  constructor() {
+    // Since this "once you go black" thing is joke, this isn't implemented properly
+    // If theming was a serious feature, the last theme would be stored to ngrx and persisted there to localstorage
+    const isDarkTheme = sessionStorage.getItem('isDarkTheme');
+    if (isDarkTheme) {
+      this.setTheme(true);
+    }
+  }
 
   setTheme(darkMode: boolean) {
     if (darkMode) {
       document.querySelector('body').classList.add('dark-theme');
+      const isDarkTheme = sessionStorage.setItem('isDarkTheme', 'yuup');
     } else {
       document.querySelector('body').classList.remove('dark-theme');
     }
